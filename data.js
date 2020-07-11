40c52bdfb767b9f1383d3c7d8f15a9cd2649a7f0
Charith <charith.suriyakula@gmail.com>
2020-07-11 11:30:17 +0530

    enabled controls

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
