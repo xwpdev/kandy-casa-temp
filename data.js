51deef1ef394a48186b50a05695bc0816ad5f6d1
Charith <charith.suriyakula@gmail.com>
2020-08-07 09:52:10 +0530

    gopro max updates

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-best_view",
-      "name": "best_view",
+      "id": "0-gs__0135",
+      "name": "GS__0135",
       "levels": [
         {
           "tileSize": 256,
@@ -22,13 +22,58 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1368,
+      "faceSize": 1440,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.21876431714128053,
+          "pitch": 0.8488944692746401,
+          "rotation": 0,
+          "target": "1-gs__0136"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gs__0136",
+      "name": "GS__0136",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.1277589385277444,
+        "pitch": 0.016355775639718573,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0243669074082087,
+          "pitch": 0.7789204345182803,
+          "rotation": 0,
+          "target": "0-gs__0135"
+        }
+      ],
       "infoHotspots": []
     }
   ],
@@ -36,7 +81,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "fullscreenButton": false,
     "viewControlButtons": false
   }
 };
