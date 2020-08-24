605a2fe0571318716dcd6f5a8b67f9d7f06f4bda
Charith <charith.suriyakula@gmail.com>
2020-08-24 21:38:01 +0530

    ground floor added

--- data.js
+++ data.js
@@ -1,8 +1,52 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-upper-lobby",
-      "name": "Upper Lobby",
+      "id": "0-main-entrance",
+      "name": "Main Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.6182126879296979,
+        "pitch": -0.019610927627271124,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6108417957038874,
+          "pitch": 0.6464545301144149,
+          "rotation": 0,
+          "target": "1-inside"
+        },
+        {
+          "yaw": 1.2343149615438982,
+          "pitch": 0.39849297415114293,
+          "rotation": 0,
+          "target": "8-balcony"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-inside",
+      "name": "Inside",
       "levels": [
         {
           "tileSize": 256,
@@ -30,17 +74,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.2066791194106834,
-          "pitch": 0.41466209793178876,
+          "yaw": -0.25073576823052157,
+          "pitch": 0.3541961647429339,
           "rotation": 0,
-          "target": "1-upper-balcony"
+          "target": "2-room-entrance"
+        },
+        {
+          "yaw": 0.09912062067331995,
+          "pitch": 0.32350704302655053,
+          "rotation": 0,
+          "target": "3-stairs"
+        },
+        {
+          "yaw": -3.0583775326226466,
+          "pitch": 0.7283234239497531,
+          "rotation": 0,
+          "target": "0-main-entrance"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-upper-balcony",
-      "name": "Upper Balcony",
+      "id": "2-room-entrance",
+      "name": "Room Entrance",
       "levels": [
         {
           "tileSize": 256,
@@ -62,26 +118,274 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -1.1277589385277444,
-        "pitch": 0.016355775639718573,
-        "fov": 1.4095469020700806
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [
         {
-          "yaw": 2.969133576832869,
-          "pitch": 0.5473200119805579,
+          "yaw": 1.2656928982088669,
+          "pitch": 0.8075718764067137,
           "rotation": 0,
-          "target": "0-upper-lobby"
+          "target": "3-stairs"
+        },
+        {
+          "yaw": 3.026357180704064,
+          "pitch": 0.2538707497835908,
+          "rotation": 0,
+          "target": "1-inside"
+        },
+        {
+          "yaw": 1.816131420664914,
+          "pitch": 0.35434850017800557,
+          "rotation": 0,
+          "target": "4-lobby"
         }
       ],
       "infoHotspots": []
+    },
+    {
+      "id": "3-stairs",
+      "name": "Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9340816112400567,
+          "pitch": 0.6744332008753826,
+          "rotation": 0,
+          "target": "2-room-entrance"
+        },
+        {
+          "yaw": 1.7055049114860417,
+          "pitch": 0.5126522435132657,
+          "rotation": 0,
+          "target": "4-lobby"
+        },
+        {
+          "yaw": 1.7985998599679265,
+          "pitch": 0.18334088422034078,
+          "rotation": 0,
+          "target": "5-dining-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-lobby",
+      "name": "Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dining-area",
+      "name": "Dining Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dining-area-2",
+      "name": "Dining Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-windows-entrance",
+      "name": "Windows Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balcony-left",
+      "name": "Balcony Left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
     }
   ],
-  "name": "Kandy Casa",
+  "name": "Kandy Casa - Ground Floor",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
